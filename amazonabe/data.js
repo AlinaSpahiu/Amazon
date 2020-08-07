5758cadae25acfbaf4d00d16da1db892f4a5cb67
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-07 23:03:02 +0200

    Create data.js

---
+++ amazonabe/data.js
@@ -0,0 +1,38 @@
+const data = {
+    products: [
+        {
+            _id:"100",
+            name: "Gray HP",
+            category: "Laptop",
+            image: "/img/d1.jpg",
+            price: 550,
+            brand: "HP",
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 0
+        },
+        {
+            _id:"101",
+            name: "Note 5",
+            category: "Smartphones",
+            image: "/img/note5.jpg",
+            price: 550,
+            brand: "Samsung",
+            rating: 4.9,
+            numReviews: 10,
+            countInStock: 8
+        },
+        {
+            _id:"102",
+            name: "Rolex for man",
+            category: "Watches",
+            image: "/img/rolex.jpg",
+            price: 390,
+            brand: "Rolex",
+            rating: 2.3,
+            numReviews: 10,
+            countInStock: 3
+        }
+    ]
+}
+module.exports = data
