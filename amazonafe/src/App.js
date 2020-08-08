853073274b3089f4112054f435dc578ebdd69951
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-08 22:12:31 +0200

    Create App.js

---
+++ amazonafe/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from "react-router-dom"
+import HomeScreen from "./components/HomeScreen"
+import ProductScreen from "./components/ProductScreen"
+import CartScreen from "./components/CartScreen"
+import Signin from "./components/Signin"
+import Register from "./components/Register"
+import Products from "./components/Products"
+import {useSelector} from "react-redux"
+
+function App() {
+
+  const userSignin = useSelector(state =>state.userSignin);
+  const {userInfo} = userSignin;
+
+  function openMenu(){
+    document.querySelector(".sidebar").classList.toggle("open")
+}
+
+  return (
+<BrowserRouter>
+  <div className="grid-container">
+    <header className="header">
+       <div className="brand">
+           <button onClick={openMenu}> &#9776; </button>
+           <Link to="/">Amazona</Link>
+       </div>
+
+       <div className="header-links">
+          <a href="cart.html">Cart</a>
+            {
+            userInfo ? <Link to="/profile">{userInfo.name}</Link>
+                     : <Link to="/signin">Sign In</Link>
+            }
+        </div>
+     </header>
+
+     <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <ul>
+            <li><a href="index.html"> Laptops</a></li>
+            <li><a href="index.html"> Smartphones</a></li>
+            <li><a href="index.html"> Watches</a></li>
+        </ul>    
+    </aside>
+
+    <main className="main">
+        <div className="content">
+          <Route path="/products" component={Products} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/register" component={Register} />
+          <Route path="/products/:id" exact={true} component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+    </main>
+
+    <footer className="footer"> All right Reserved </footer>
+ </div>
+</BrowserRouter>
+  );
+}
+
+export default App;
