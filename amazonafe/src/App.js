8644d79a9584aeade3617ac706cacdcbadeb3b7d
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-20 21:56:23 +0200

    Update App.js

--- amazonafe/src/App.js
+++ amazonafe/src/App.js
@@ -6,6 +6,9 @@ import CartScreen from "./components/CartScreen"
 import Signin from "./components/Signin"
 import Register from "./components/Register"
 import Products from "./components/Products"
+import Shipping from "./components/Shipping"
+import Payment from "./components/Payment"
+import PlaceOrder from "./components/PlaceOrder"
 import {useSelector} from "react-redux"
 
 function App() {
@@ -47,6 +50,9 @@ function App() {
     <main className="main">
         <div className="content">
           <Route path="/products" component={Products} />
+          <Route path="/shipping" component={Shipping} />
+          <Route path="/payment" component={Payment} />
+          <Route path="/placeorder" component={PlaceOrder} />
           <Route path="/signin" component={Signin} />
           <Route path="/register" component={Register} />
           <Route path="/products/:id" exact={true} component={ProductScreen} />
