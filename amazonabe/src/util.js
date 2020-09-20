bb21d8151557e588f814f41784b69700c82d7936
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-20 21:56:04 +0200

    Update util.js

--- amazonabe/src/util.js
+++ amazonabe/src/util.js
@@ -1,40 +1,45 @@
-const { JsonWebTokenError } = require("jsonwebtoken")
-
-const jwt = require("jsonwebtoken");
-
-
+const jwt = require( 'jsonwebtoken');
+const config = require( '../config');
+const { JWT_SECRET } = require('../config');
 const getToken = (user) => {
-    return jwt.sign({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-    }, 'somethingelsesecret', {
-        expiresIn: '48h'
-    })
-}
-
-const isAuth = (req, res, next) =>{
-    const token =req.headers.authorization;
-    if(token){
-        const onlyToken = token.slice(7, token.length)
-        jwt.verify(onlyToken, process.env.JWT_SECRET || 'somethingsecret',(err, decode) =>{
-            if(err){
-                return res.status(401).send({msg: 'Invalid Token!'})
-            }
-            req.user =token;
-            next()
-            return
-        } )
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: '48h',
     }
-    return res.status(401).send({msg: "Token is not suplied."})
-}
+  );
+};
+
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const onlyToken = token.slice(7, token.length);
+    jwt.verify(onlyToken, JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({ message: 'Invalid Tokennnnnnnn' });
+      }
+      req.user = decode;
+      next();
+      return;
+    });
+  } else {
+    return res.status(401).send({ message: 'Token is not supplied.' });
+  }
+};
 
 const isAdmin = (req, res, next) => {
-    if(req.user && req.user.isAdmin) {
-        return next()
-    }
-    return res.status(401).send({msg: "Admin token is not valid."})
-}
+  console.log(req.user);
+  if (req.user && req.user.isAdmin) {
+    return next();
+  }
+  return res.status(401).send({ message: 'Admin Token is not valid.' });
+};
 
-module.exports= {getToken, isAuth, isAdmin};
+module.exports= { getToken, isAuth, isAdmin };
