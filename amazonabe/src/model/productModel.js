82c428d913475c385abf2c60bd1897b5fe24503d
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-20 21:55:49 +0200

    Update productModel.js

--- amazonabe/src/model/productModel.js
+++ amazonabe/src/model/productModel.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema({
     name:         { type: String, required: true},
     image:        { type: String, required: true},
     brand:        { type: String, required: true},
-    price:        { type: Number, required: true, default: 0},
+    price:        { type: Number, default: 0 , required: true},
     category:     { type: String, required: true},
     countInStock: { type: Number, required: true, default: 0},
     description:  { type: String, required: true},
@@ -13,6 +13,6 @@ const productSchema = new mongoose.Schema({
     numReviews:   { type: Number, required: true, default: 0},
 })
 
-const productModel = mongoose.model("product", productSchema);
+const productModel = mongoose.model("Product", productSchema);
 
 module.exports= productModel;
