57907ab9fa41cc7e713dfc254a64a2071bfba0d3
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-11-09 22:58:33 +0100

    Update userActions.js

--- amazonafe/src/actions/userActions.js
+++ amazonafe/src/actions/userActions.js
@@ -17,7 +17,7 @@ const signin = (email, password) => async(dispatch) =>{
 
 }
 
-
+// User Register Action
 const register = (name, email, password) => async(dispatch) =>{
 
     dispatch(  { type:USER_REGISTER_REQUEST, payload: {name, email, password}  }  );
